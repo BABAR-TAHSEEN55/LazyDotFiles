6e3e246baa6365d5a92a363d9d1403373ba8e1a6
Sizzler_55 <babarsmoke24@gmail.com>
2024-12-12 20:44:56 +0530

    Neotree , lsp , Colorscheme SetUp

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,48 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- Show hidden files by default
+        hide_dotfiles = false, -- Ensure dotfiles like .env are visible
+        hide_by_name = { -- Optional: remove any specific filtering of .env
+          -- "node_modules", ".git", ".env" -- remove ".env" from this list
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("neo-tree").setup(opts)
+
+    -- Add custom highlights for Neo-tree
+    vim.cmd([[
+      " Set Neo-tree background to transparent
+      highlight NeoTreeNormal guibg=NONE ctermbg=NONE
+      highlight NeoTreeNormalNC guibg=NONE ctermbg=NONE
+      highlight NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+      highlight WinSeparator guibg=NONE guifg=#444444
+
+      " Set directory and file colors to Solarized theme
+      highlight NeoTreeDirectoryName guifg=#268BD2
+      highlight NeoTreeDirectoryIcon guifg=#268BD2
+      highlight NeoTreeFileName guifg=#839496
+      highlight NeoTreeFileNameOpened guifg=#B58900
+
+      " Customize Git status colors (optional)
+      highlight NeoTreeGitAdded guifg=#859900
+      highlight NeoTreeGitModified guifg=#B58900
+      highlight NeoTreeGitDeleted guifg=#DC322F
+
+      " Fix transparent background rendering issue (add this)
+      highlight Normal guibg=NONE
+      highlight NonText guibg=NONE
+    ]])
+
+    vim.api.nvim_create_autocmd({ "BufWinEnter", "BufWinLeave" }, {
+      pattern = "*Neo-tree*",
+      callback = function()
+        vim.cmd("redraw!")
+      end,
+    })
+  end,
+}
