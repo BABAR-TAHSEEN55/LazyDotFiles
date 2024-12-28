9a17fe7d26984d996352b2d32f0d5a6a2e64b98e
Sizzler_55 <babarsmoke24@gmail.com>
2024-12-28 12:28:02 +0530

    UI Tweaks

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,18 @@
+return {
+
+  {
+    "kdheepak/lazygit.nvim",
+    keys = {
+      {
+        ";c",
+        ":LazyGit<Return>",
+        silent = true,
+        noremap = true,
+      },
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+}
