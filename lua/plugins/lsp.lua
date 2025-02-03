8bc20781e5e662a698a81feb9675cfb5039272e2
Babar <babarsmoke24@gmail.com>
2025-02-03 19:30:39 +0530

    nvim=cmp Refactored

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -132,12 +132,60 @@ return {
     "imsnif/kdl.vim",
     ft = "kdl",
   },
-
+  -- {
+  --   "rafamadriz/friendly-snippets",
+  --   config = function()
+  --     require("luasnip").filetype_extend("javascriptreact", { "html" })
+  --     require("luasnip.loaders.from_vscode").lazy_load()
+  --   end,
+  -- },
   {
     "nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
+    dependencies = { "hrsh7th/cmp-emoji", enabled = false },
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer", -- source for text in buffer
+      "hrsh7th/cmp-path", -- source for file system paths
+      "hrsh7th/cmp-cmdline", -- source for command line completion
+      "hrsh7th/cmp-nvim-lsp", -- source for LSP completion
+      "L3MON4D3/LuaSnip", -- snippet engine
+      "rafamadriz/friendly-snippets", -- snippets
+      "onsails/lspkind-nvim", -- icons for nvim-cmp
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
     end,
   },
 }
